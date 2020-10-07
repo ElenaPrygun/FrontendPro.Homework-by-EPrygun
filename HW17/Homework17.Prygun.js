8df02ba2f88a8523e6e7bd5c6c6e036945607246
Prygun <elena.prygun@gmail.com>
2020-10-07 16:17:27 +0300

    hw17update

--- HW17/Homework17.Prygun.js
+++ HW17/Homework17.Prygun.js
@@ -1,61 +1,72 @@
-const wrapper = document.querySelector("#wrapper");
-const divForParagraphs = document.createElement("div");
+document.addEventListener("DOMContentLoaded", function () {
+  const wrapper = document.querySelector("#wrapper");
+  const divForParagraphs = document.createElement("div");
 
-const input = document.createElement("input");
-input.style.margin = "20px";
-input.placeholder = "Enter text";
+  const KEY = "User name:";
+  let itemsArray = readData();
+  saveData(itemsArray);
 
-const saveButton = document.createElement("button");
-saveButton.innerHTML = "SAVE";
+  try {
+    readData();
+  } catch (e) {
+    console.error("Error in readData", e);
+  }
 
-const KEY = "User name:";
+  const input = document.createElement("input");
+  input.style.margin = "20px";
+  input.placeholder = "Enter text";
 
-let itemsArray = readData();
-saveData(itemsArray);
+  const saveButton = document.createElement("button");
+  saveButton.innerHTML = "SAVE";
 
-const data = JSON.parse(localStorage.getItem(KEY));
+  const data = readData();
 
-const paragraphMaker = (text) => {
-  const paragraph = document.createElement("p");
-  const span = document.createElement("span");
-  span.innerHTML = text;
-  paragraph.appendChild(span);
+  const paragraphMaker = (text) => {
+    const paragraph = document.createElement("p");
+    const span = document.createElement("span");
+    span.innerHTML = text;
+    paragraph.appendChild(span);
 
-  const delButton = document.createElement("button");
-  delButton.innerHTML = "Delete";
-  delButton.style.margin = "20px";
+    const delButton = document.createElement("button");
+    delButton.innerHTML = "Delete";
+    delButton.style.margin = "20px";
 
-  delButton.addEventListener("click", () => {
-    paragraph.remove();
-    itemsArray = Array.from(divForParagraphs.children).map(
-      (child) => child.querySelectorAll("span")[0].innerText
-    );
+    delButton.addEventListener("click", () => {
+      paragraph.remove();
+
+      let arrayOfSpan = document.querySelectorAll("span");
+      let itemsArrayText = Array.from(arrayOfSpan).map(
+        (item) => item.innerText
+      );
+      saveData(itemsArrayText);
+    });
+
+    paragraph.appendChild(delButton);
+    divForParagraphs.appendChild(paragraph);
+  };
+
+  saveButton.addEventListener("click", () => {
+    itemsArray.push(input.value);
     saveData(itemsArray);
+    paragraphMaker(input.value);
+    input.value = "";
   });
 
-  paragraph.appendChild(delButton);
-  divForParagraphs.appendChild(paragraph);
-};
+  data.forEach((item) => {
+    paragraphMaker(item);
+  });
 
-saveButton.addEventListener("click", () => {
-  itemsArray.push(input.value);
-  saveData(itemsArray);
-  paragraphMaker(input.value);
-  input.value = "";
+  function saveData(arrayToSave) {
+    localStorage.setItem(KEY, JSON.stringify(arrayToSave));
+  }
+
+  function readData() {
+    return localStorage.getItem(KEY)
+      ? JSON.parse(localStorage.getItem(KEY))
+      : [];
+  }
+
+  wrapper.appendChild(input);
+  wrapper.appendChild(saveButton);
+  wrapper.appendChild(divForParagraphs);
 });
-
-data.forEach((item) => {
-  paragraphMaker(item);
-});
-
-function saveData(arrayToSave) {
-  localStorage.setItem(KEY, JSON.stringify(arrayToSave));
-}
-
-function readData() {
-  return localStorage.getItem(KEY) ? JSON.parse(localStorage.getItem(KEY)) : [];
-}
-
-wrapper.appendChild(input);
-wrapper.appendChild(saveButton);
-wrapper.appendChild(divForParagraphs);
